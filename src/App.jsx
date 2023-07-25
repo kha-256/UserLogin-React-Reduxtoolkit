4cccdaf44d42db7c5b331e0a8a10bf344500b2a9
khadija <khadijanoorhashmi@gmail.com>
2023-07-25 23:45:40 +0500

    Sigup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import './App.css'
 import { Routes,Route } from 'react-router-dom'
-import Home from './Pages/Home'
 import Login from './Pages/Login'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Signup from './Pages/Signup';
 
 function App() {
   
 
   return (
     <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path="/login" element={<Login/>} />
+      <Route path="/" element={<Login/>} />
+      <Route path="/signup" element={<Signup/>} />
+
      
     </Routes>
   )
