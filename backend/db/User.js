0f3c12303b8976b550b8e7a4fdf021ca633c5f8c
khadija <khadijanoorhashmi@gmail.com>
2023-07-25 23:44:29 +0500

    Sigup page

--- backend/db/User.js
+++ backend/db/User.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 
 const userSchema= new mongoose.Schema({
-    name:String,
-    password:String
-})
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+},{ versionKey: false })
 
 module.exports= mongoose.model("users",userSchema);
